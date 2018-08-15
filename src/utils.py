7a7e2746909e2493486127030e0fa4a0f21a1cd3
SimGus <simon.gustin@hotmail.com>
2018-08-15 17:12:52 +0200

    Add print debug utility

---
+++ src/utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def printDBG(txt):
+    print("[DBG] "+txt)
+
+if __name__ == "__main__":
+    import warnings
+    warnings.warn("You are running the wrong file ('utils.py')." +
+        "The file that should be run is 'main.py'.")
