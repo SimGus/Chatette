d7db1beaf637dfc8610206a9e9865a0be2021cda
SimGus <simon.gustin@hotmail.com>
2018-08-17 16:59:15 +0200

    Put output file in the same directory as the input file (this may change in the future)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, io
+import sys, os, io
 from Parser import Parser, Unit
 from Generator import Generator
 
@@ -17,18 +17,19 @@ if __name__ == "__main__":
     if len(sys.argv) <= 1 or len(sys.argv) > 3:
         print_usage()
     else:
-        template_filename = sys.argv[1]
+        template_file_path = sys.argv[1]
         output_filename = DEFAULT_OUTPUT_FILENAME
         if len(sys.argv) == 3:
             output_filename = sys.argv[2]
+        output_file_path = os.path.join(os.path.dirname(template_file_path), output_filename)
 
         parser = None
-        with open(template_filename, 'r') as in_file:
+        with open(template_file_path, 'r') as in_file:
             parser = Parser(in_file)
             parser.parse()
             parser.printDBG()
         print("")
 
-        with io.open(output_filename, 'w+', encoding="utf-8") as out_file:  # TODO create if not already existing (same with dirs)
+        with io.open(output_file_path, 'w+', encoding="utf-8") as out_file:  # TODO create if not already existing (same with dirs)
             generator = Generator(out_file, parser)
             generator.generate()
