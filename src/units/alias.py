290b929a02c1c8b57b7f620482d714344ebd4042
SimGus <simon.gustin@hotmail.com>
2018-08-22 23:24:13 +0200

    Add a beginning of implementation of aliases

---
+++ src/units/alias.py
@@ -0,0 +1,16 @@
+from units import *
+
+class AliasDefinition(UnitDefinition):
+    """
+    This class represents the definition of an alias,
+    containing all the rules that it can represent.
+    """
+    pass  # TODO
+
+
+class AliasModel(UnitModel):
+    """
+    This class represent an alias as it can be contained
+    in a rule with its modifiers.
+    """
+    pass  # TODO
