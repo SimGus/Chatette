6dfc6e878c6994ba7a1fc8449e353ec4af22d566
SimGus <simon.gustin@hotmail.com>
2018-08-24 11:45:19 +0200

    Change generator to be well included inside the OOP remake

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python3
 
+import sys
+
+
 def printDBG(txt):
     print("[DBG] "+txt)
+
+
+def cast_to_unicode(any):
+    if sys.version_info[0] == 3:
+        return any
+    if isinstance(any, str):
+        return unicode(any, "utf-8")
+    elif isinstance(any, dict):
+        cast_dict = dict()
+        for key in any:
+            cast_key = cast_to_unicode(key)
+            cast_value = cast_to_unicode(any[key])
+            cast_dict[cast_key] = cast_value
+        return cast_dict
+    elif isinstance(any, list):
+        cast_list = []
+        for e in any:
+            cast_list.append(cast_to_unicode(e))
+        return cast_list
+    else:
+        return any
+
 
 if __name__ == "__main__":
     import warnings
