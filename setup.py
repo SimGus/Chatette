931d286db1d06e26fc1a6017e80aeb8dca48af9b
SimGus <simon.gustin@hotmail.com>
2018-09-19 15:10:00 +0200

    Add information in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,19 @@ from setuptools import find_packages, setup
 # https://the-hitchhikers-guide-to-packaging.readthedocs.io/en/latest/quickstart.html
 
 setup(
-    name='chatette',
-    version='1.1.5',
+    name="chatette",
+    version="1.1.5",
+    description="A dataset generator for Rasa NLU",
+    author="SimGus",
+    license="MIT",
+    url="https://github.com/SimGus/Chatette",
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description=open(join(dirname(__file__), "README.md")).read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
