c4731e1ad81c199eb8c2887120b12ffb8e8c2858
SimGus <simon.gustin@hotmail.com>
2018-10-22 11:35:35 +0200

    Add classifiers for PyPI package

--- setup.py
+++ setup.py
@@ -20,5 +20,10 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Topic :: Text Processing",
+        "Topic :: Communications",
     ]
 )
