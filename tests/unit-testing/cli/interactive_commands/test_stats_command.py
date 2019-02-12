7a6104cc1e5acdab38b8bbf1f7ff8d9d3f6fdb7c
SimGus <simon.gustin@hotmail.com>
2019-02-12 16:00:43 +0100

    Add unit tests for 'stats' command

---
+++ tests/unit-testing/cli/interactive_commands/test_stats_command.py
@@ -0,0 +1,30 @@
+"""
+Test module
+Tests the functionalities that are contained in module
+`chatette.cli.interactive_commands.stats_command`.
+"""
+
+from chatette.cli.interactive_commands.command_strategy import CommandStrategy
+from chatette.cli.interactive_commands.stats_command import StatsCommand
+
+from test_command_strategy import get_facade
+
+
+def test_obj():
+    cmd = StatsCommand("")
+    assert isinstance(cmd, CommandStrategy)
+    assert cmd.command_tokens == []
+
+    cmd = StatsCommand("stats")
+    assert isinstance(cmd, CommandStrategy)
+    assert cmd.command_tokens == ["stats"]
+
+
+def test_execute(capsys):
+    cmd = StatsCommand('stats')
+    assert cmd.command_tokens == ["stats"]
+    facade = get_facade()
+    cmd.execute(facade)
+    captured = capsys.readouterr()
+    assert "Statistics:\n\t1 files parsed\n\t7 declarations: " + \
+           "1 intents, 2 slots and 4 aliases\n\t22 rules" in captured.out
