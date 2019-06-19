e8dde58e86fefd117e7c819240b2ac5eb3feb58f
SimGus <simon.gustin@hotmail.com>
2019-06-19 19:42:23 +0200

    Make a folder a module

---
+++ chatette/new_units/unit_definitions/__init__.py
@@ -0,0 +1,6 @@
+"""
+Module `chatette.new_units.unit_definitions`
+Contains the class meant to represent the definitions of units
+(aliases, slots and intents) and the logic associated to the generation
+of examples they are associated with.
+"""
