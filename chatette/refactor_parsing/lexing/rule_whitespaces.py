3b940996b5d10eb190fabe61810dbdaac1ee6ab1
SimGus <simon.gustin@hotmail.com>
2019-08-11 12:44:21 +0200

    Make lexical rule for whitespaces also parse indentations

--- chatette/refactor_parsing/lexing/rule_whitespaces.py
+++ chatette/refactor_parsing/lexing/rule_whitespaces.py
@@ -11,6 +11,17 @@ from chatette.refactor_parsing.lexing import LexicalToken, TerminalType
 
 class RuleWhitespaces(LexingRule):
     def _apply_strategy(self, **kwargs):
+        """
+        `kwargs` can contain a boolean at key `parsing_indentation` that is
+        `True` iff the whitespaces currently parsed correspond to
+        an indentation, and `False` if it corresponds to simple whitespaces.
+        If `kwargs` doesn't contain this boolean, defaults to `False`.
+        """
+        parsing_indentation = kwargs.get("parsing_indentation", False)
+        if parsing_indentation:
+            terminal_type = TerminalType.indentation
+        else:
+            terminal_type = TerminalType.whitespace
         text = self._text
 
         while self._next_index < len(text) and text[self._next_index].isspace():
@@ -18,9 +29,7 @@ class RuleWhitespaces(LexingRule):
             self._update_furthest_matched_index()
         if self._next_index > self._start_index:
             matched_text = text[self._start_index:self._next_index]
-            self._tokens.append(
-                LexicalToken(TerminalType.whitespace, matched_text)
-            )
+            self._tokens.append(LexicalToken(terminal_type, matched_text))
             return True
         
         self.error_msg = "Invalid token. Expected at least one whitespace there."
