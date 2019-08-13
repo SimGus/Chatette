052911d51ff1970a2c5706e45dc9dc8c226eeade
SimGus <simon.gustin@hotmail.com>
2019-08-14 01:05:46 +0200

    Change new unit references to work with new AST API

--- chatette/refactor_units/unit_reference.py
+++ chatette/refactor_units/unit_reference.py
@@ -20,21 +20,38 @@ class UnitReference(GeneratingItem):
     def __init__(self, name, unit_type):
         self._unit_type = unit_type
         super(UnitReference, self).__init__(name)
-        # TODO not added yet
-        # self._definition = AST.get_or_create()[unit_type.value][name]
+        try:
+            self._definition = AST.get_or_create()[unit_type.value][name]
+        except KeyError:
+            self._definition = None
+        
     
     def _compute_full_name(self):
         return "reference to " + self._unit_type.value + " '" + \
             self._name + "'"
     
+
+    def get_definition(self):
+        if self._definition is None:
+            try:
+                self._definition = \
+                    AST.get_or_create()[self._unit_type.value][self._name]
+            except KeyError:
+                raise KeyError(
+                    "Couldn't find the definition corresponding to " + \
+                    self.full_name + "."
+                )
+        return self._definition
+
+    
     def _compute_nb_possibilities(self):
-        return self._definition.get_max_nb_possibilities()
+        return self.get_definition().get_max_nb_possibilities()
     
     def _generate_random_strategy(self):
-        return self._definition.generate_random()
+        return self.get_definition().generate_random()
     
     def _generate_all_strategy(self):
-        return self._definition.generate_all()
+        return self.get_definition().generate_all()
     
     def _generate_n_strategy(self, n):
-        return self._definition.generate_nb_possibilities(n)
+        return self.get_definition().generate_nb_possibilities(n)
