6b8478c68aae94094f81f26c52f01c5a8a63b4b2
SimGus <simon.gustin@hotmail.com>
2019-08-20 19:02:10 +0200

    Start implement support for random generation names in randgen

--- chatette/modifiers/randgen.py
+++ chatette/modifiers/randgen.py
@@ -10,6 +10,9 @@ from random import randrange
 from chatette.refactor_units import add_example_no_dup
 
 
+RANDGEN_MAPPING_KEY = "randgen_mapping"
+
+
 def modify_nb_possibilities(unmodified_nb_possibilities):
     """
     Returns the number of possibilities of generation for an item that has
@@ -34,10 +37,35 @@ def should_generate(randgen_name, percentgen, randgen_mappings=None):
     return randgen_mappings[randgen_name]
 
 
-def make_all_possibilities(examples, empty_example):
+def make_all_possibilities(examples, empty_example, randgen_name=None):
     """
     Given the list of examples `examples`, constructs and returns a list
     of all possible examples after the random generation modifier applied.
+    Updates the random generation mapping for each example, considering
+    that each example in `examples` was generated with a mapping such that
+    `randgen_name` was associated to `True`.
     `empty_example` is a new example that has an empty text and no entities.
+    @raises: - `KeyError` if `randgen_name` is already present in a random
+               generation mapping.
     """
+    if randgen_name is not None:
+        for ex in examples:
+            current_randgen_mapping = getattr(ex, RANDGEN_MAPPING_KEY, dict())
+            if randgen_name in current_randgen_mapping:
+                raise KeyError(
+                    "Didn't expect the random generation name '" + randgen_name + \
+                    "' to already be set."
+                )
+            current_randgen_mapping[randgen_name] = True
+            setattr(ex, RANDGEN_MAPPING_KEY, current_randgen_mapping)
+        
+        current_randgen_mapping = \
+            getattr(empty_example, RANDGEN_MAPPING_KEY, dict())
+        if randgen_name in current_randgen_mapping:
+            raise KeyError(
+                "Didn't expect the random generation name '" + randgen_name + \
+                "' to already be set."
+            )
+        setattr(empty_example, RANDGEN_MAPPING_KEY, current_randgen_mapping)
+
     return add_example_no_dup(examples, empty_example)
