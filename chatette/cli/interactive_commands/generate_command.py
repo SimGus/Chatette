bfaab2e0b7b2b5547faa3832783e5cbfafa08cc8
SimGus <simon.gustin@hotmail.com>
2019-08-28 15:29:52 +0200

    Fix 'generate' command

--- chatette/cli/interactive_commands/generate_command.py
+++ chatette/cli/interactive_commands/generate_command.py
@@ -8,6 +8,9 @@ given adapter.
 from chatette.cli.interactive_commands.command_strategy import CommandStrategy
 from chatette.adapters.factory import create_adapter
 
+from chatette.facade import Facade
+from chatette.refactor_units.ast import AST
+
 
 class GenerateCommand(CommandStrategy):
     usage_str = 'generate <adapter> <unit-type> "<unit-name>"'
@@ -15,11 +18,17 @@ class GenerateCommand(CommandStrategy):
         super(GenerateCommand, self).__init__(command_str, quiet)
         self.nb_examples = None
 
-    def execute(self, facade):
+    def execute(self):
         """
         Implements the command `generate` which generates all possible examples
         of a certain unit, formatted according to a certain adapter.
         """
+        if not Facade.was_instantiated():
+            self.print_wrapper.error_log(
+                "Cannot generate anything. No file was parsed."
+            )
+            return
+        facade = Facade.get_or_create()
         if len(self.command_tokens) == 1:
             facade.run_generation()
             return
@@ -27,79 +36,96 @@ class GenerateCommand(CommandStrategy):
             try:
                 facade.run_generation(self.command_tokens[1])
             except ValueError:
-                self.print_wrapper.write("Unknown adapter: '" +
-                                         self.command_tokens[1] + "'")
+                self.print_wrapper.write(
+                    "Unknown adapter: '" + self.command_tokens[1] + "'"
+                )
             return
         if len(self.command_tokens) < 4:
-            self.print_wrapper.error_log("Missing some arguments\nUsage: " +
-                                         self.usage_str)
+            self.print_wrapper.error_log(
+                "Missing some arguments\nUsage: " + self.usage_str
+            )
             return
 
         adapter_str = self.command_tokens[1]
         try:
             adapter = create_adapter(adapter_str)
         except ValueError:
-            self.print_wrapper.error_log("Unknown adapter '" + adapter_str + \
-                                         "'.")
+            self.print_wrapper.error_log(
+                "Unknown adapter '" + adapter_str + "'."
+            )
 
         if len(self.command_tokens) == 5:
             try:
                 self.nb_examples = int(self.command_tokens[-1])
             except ValueError:
-                self.print_wrapper.error_log("The number of examples to be " +
-                                             "generated is invalid: it must " +
-                                             "be an integer (no other " +
-                                             "characters allowed).")
+                self.print_wrapper.error_log(
+                    "The number of examples to be generated is invalid: " + \
+                    "it must be an integer (no other characters allowed)."
+                )
                 return
 
-        unit_type = CommandStrategy.get_unit_type_from_str(self.command_tokens[2])
+        unit_type = \
+            CommandStrategy.get_unit_type_from_str(self.command_tokens[2])
         if unit_type is None:
-            self.print_wrapper.error_log("Unknown unit type: '" +
-                                         str(self.command_tokens[2]) + "'.")
+            self.print_wrapper.error_log(
+                "Unknown unit type: '" + str(self.command_tokens[2]) + "'."
+            )
             return
 
         unit_regex = self.get_regex_name(self.command_tokens[3])
         if unit_regex is None:
+            print("no regex")
             try:
                 [unit_name, variation_name] = \
-                    CommandStrategy.split_exact_unit_name(self.command_tokens[3])
+                    CommandStrategy.split_exact_unit_name(
+                        self.command_tokens[3]
+                    )
             except SyntaxError:
-                self.print_wrapper.error_log("Unit identifier couldn't be " + \
-                                             "interpreted. Did you mean to " + \
-                                             "escape some hashtags '#'?")
+                self.print_wrapper.error_log(
+                    "Unit identifier couldn't be interpreted. " + \
+                    "Did you mean to escape some hashtags '#'?"
+                )
                 return
-            self._generate_unit(facade, adapter, unit_type, unit_name,
-                                variation_name)
+            self._generate_unit(
+                adapter, unit_type, unit_name, variation_name
+            )
         else:
             count = 0
-            for unit_name in self.next_matching_unit_name(facade.parser,
-                                                          unit_type,
-                                                          unit_regex):
-                self._generate_unit(facade, adapter, unit_type, unit_name)
+            for unit_name in self.next_matching_unit_name(
+                facade.parser, unit_type, unit_regex
+            ):
+                self._generate_unit(adapter, unit_type, unit_name)
                 count += 1
             if count == 0:
                 self.print_wrapper.write("No " + unit_type.name + " matched.")
-        self.finish_execution(facade)
+        self.finish_execution()
 
-    def _generate_unit(self, facade, adapter, unit_type, unit_name,
-                       variation_name=None):
-        definition = facade.parser.ast.get_definition(unit_name, unit_type)
-        examples = definition.generate_nb_examples(self.nb_examples,
-                                                   variation_name)
+    def _generate_unit(self,
+        adapter, unit_type, unit_name, variation_name=None
+    ):
+        definition = AST.get_or_create()[unit_type][unit_name]
+        if self.nb_examples is None:
+            examples = definition.generate_all(variation_name=variation_name)
+        else:
+            examples = definition.generate_nb_possibilities(
+                self.nb_examples, variation_name=variation_name
+            )
 
-        self.print_wrapper.write("Generated examples for " + unit_type.name +
-                                 " '" + unit_name + "':'")
+        self.print_wrapper.write(
+            "Generated examples for " + unit_type.name + " '" + unit_name + \
+            "':'"
+        )
         for ex in examples:
             # HACK: add a name of intent as ex is not especially a IntentExample
-            if not hasattr(ex, 'name'):
-                ex.name = "INTERACTIVE"
+            if not hasattr(ex, 'intent_name'):
+                ex.intent_name = "INTERACTIVE"
             self.print_wrapper.write(adapter.prepare_example(ex))
         self.print_wrapper.write("")
 
-    def finish_execution(self, facade):
+    def finish_execution(self):
         self.nb_examples = None
 
 
     # Override abstract methods
-    def execute_on_unit(self, facade, unit_type, unit_name, variation_name=None):
+    def execute_on_unit(self, unit_type, unit_name, variation_name=None):
         raise NotImplementedError()
