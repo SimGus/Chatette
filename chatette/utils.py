b666f711d064dc3363fdfd09ac0a1b2ebd48c474
SimGus <simon.gustin@hotmail.com>
2019-08-29 01:38:04 +0200

    Don't give arguments to 'object' constructor as it is not allowed in python 3

--- chatette/utils.py
+++ chatette/utils.py
@@ -33,7 +33,7 @@ class Singleton(object):
     _instance = None
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
     @classmethod
