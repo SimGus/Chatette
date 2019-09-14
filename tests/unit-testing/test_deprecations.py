93f17754e12b152d9e2dff73e7b157cd0d6b05c0
SimGus <simon.gustin@hotmail.com>
2019-09-14 10:48:55 +0200

    Make tests for deprecation warnings more thorough

--- tests/unit-testing/test_deprecations.py
+++ tests/unit-testing/test_deprecations.py
@@ -27,6 +27,28 @@ class TestDeprecations(object):
         captured = capsys.readouterr()
         assert "Comments starting with a semi-colon" in captured.err
 
+        instance = Deprecations.reset_instance()
+        instance.warn_old_comment("filename")
+
+        captured = capsys.readouterr()
+        assert "This syntax was found in file 'filename'." in captured.err
+
+        instance = Deprecations.reset_instance()
+        instance.warn_old_comment("filename", 12, "line")
+
+        captured = capsys.readouterr()
+        assert \
+            "This syntax was found in file 'filename'" + \
+            " at line 12: 'line'." in captured.err
+        
+        instance = Deprecations.reset_instance()
+        instance.warn_old_comment(line_nb=12, line="line")
+
+        captured = capsys.readouterr()
+        assert \
+            "This syntax was found" + \
+            " at line 12: 'line'." in captured.err
+
     def test_warn_old_choice(self, capsys):
         instance = Deprecations.reset_instance()
         assert not instance._old_comment_warned
@@ -38,3 +60,26 @@ class TestDeprecations(object):
 
         captured = capsys.readouterr()
         assert "Choices starting with " in captured.err
+
+        instance = Deprecations.reset_instance()
+        instance.warn_old_choice("filename")
+
+        captured = capsys.readouterr()
+        assert "This syntax was found in file 'filename'." in captured.err
+
+        instance = Deprecations.reset_instance()
+        instance.warn_old_choice("filename", 12, "line")
+
+        captured = capsys.readouterr()
+        assert \
+            "This syntax was found in file 'filename'" + \
+            " at line 12: 'line'." in captured.err
+        
+        instance = Deprecations.reset_instance()
+        instance.warn_old_choice(line_nb=12, line="line")
+
+        captured = capsys.readouterr()
+        assert \
+            "This syntax was found" + \
+            " at line 12: 'line'." in captured.err
+
