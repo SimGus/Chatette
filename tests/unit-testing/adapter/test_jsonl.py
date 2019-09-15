a020629f80451f3fec413ebbd9d9150211011552
SimGus <simon.gustin@hotmail.com>
2019-09-15 22:49:08 +0200

    Add some unit tests for JSONL adapter

--- tests/unit-testing/adapter/test_jsonl.py
+++ tests/unit-testing/adapter/test_jsonl.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 """
 Test module.
 Tests the functionalities present in module
@@ -7,4 +8,16 @@ Tests the functionalities present in module
 from chatette.adapters.jsonl import JsonListAdapter
 
 
+class TestJsonListAdapter(object):
+    def test_constructor(self):
+        adapter = JsonListAdapter()
+        assert adapter._batch_size == 10000
+        assert adapter._base_filepath is None
 
+        adapter = JsonListAdapter("path", 10)
+        assert adapter._batch_size == 10
+        assert adapter._base_filepath == "path"
+    
+    def test_file_extension(self):
+        assert JsonListAdapter()._get_file_extension() == "jsonl"
+    
