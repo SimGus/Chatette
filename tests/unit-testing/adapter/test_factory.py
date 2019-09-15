6ede7a79e01c3c4c4f6ec17b37a0d0c3c5d9f927
SimGus <simon.gustin@hotmail.com>
2019-09-15 13:16:50 +0200

    Adapt unit tests for adapter factory to also test new adapter

--- tests/unit-testing/adapter/test_factory.py
+++ tests/unit-testing/adapter/test_factory.py
@@ -9,11 +9,14 @@ import pytest
 from chatette.adapters.factory import create_adapter
 from chatette.adapters.jsonl import JsonListAdapter
 from chatette.adapters.rasa import RasaAdapter
+from chatette.adapters.rasa_md import RasaMdAdapter
 
 
 def test_valid():
     assert isinstance(create_adapter("jsonl"), JsonListAdapter)
     assert isinstance(create_adapter("rasa"), RasaAdapter)
+    assert isinstance(create_adapter("rasa-md"), RasaMdAdapter)
+    assert isinstance(create_adapter("rasamd"), RasaMdAdapter)
 
 def test_invalid():
     assert create_adapter(None) is None
