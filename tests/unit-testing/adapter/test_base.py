0145572c3084f2d2845da8de47360a954340909b
SimGus <simon.gustin@hotmail.com>
2019-09-17 22:54:48 +0200

    Add a unit test to test class method in abstract adapter

--- tests/unit-testing/adapter/test_base.py
+++ tests/unit-testing/adapter/test_base.py
@@ -22,3 +22,16 @@ class TestAdapter(object):
     def test_abstract(self):
         with pytest.raises(TypeError):
             adapter = Adapter()
+    
+    def test_generate_batch(self):
+        for batch in Adapter._Adapter__generate_batch(["ex"], [], None):
+            assert batch.index == 0
+            assert len(batch.examples) == 1
+            assert batch.examples[0] == "ex"
+            assert len(batch.synonyms) == 0
+
+        for batch in Adapter._Adapter__generate_batch(["a", "b"], [], 1):
+            assert batch.index in (0, 1)
+            assert len(batch.examples) == 1
+            assert batch.examples[0] in ("a", "b")
+            assert len(batch.synonyms) == 0
