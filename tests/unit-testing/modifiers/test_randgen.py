464ae19e48aaf7abccf111e98645493798b541eb
SimGus <simon.gustin@hotmail.com>
2019-09-23 22:01:54 +0200

    Thoroughly test random generation functionalities

--- tests/unit-testing/modifiers/test_randgen.py
+++ tests/unit-testing/modifiers/test_randgen.py
@@ -6,6 +6,8 @@ Tests the functionalities that are present in module
 """
 
 
+import pytest
+
 from chatette.modifiers.randgen import *
 from chatette.units import Example
 
@@ -51,6 +53,26 @@ class TestMakeAllPossibilities(object):
                 assert not current_mapping["randgen"]
             else:
                 assert current_mapping["randgen"]
+    
+    def test_errors(self):
+        empty = Example()
+        examples = [Example("test1"), Example("test2")]
+        setattr(examples[0], RANDGEN_MAPPING_KEY, {"name": False})
+
+        with pytest.raises(KeyError):
+            make_all_possibilities(examples, empty, "name")
+
+        examples[0] = Example("test1")
+        setattr(examples[1], RANDGEN_MAPPING_KEY, {"name": True})
+
+        with pytest.raises(KeyError):
+            make_all_possibilities(examples, empty, "name")
+
+        examples[1] = Example("test2")
+        setattr(empty, RANDGEN_MAPPING_KEY, {"name": True})
+
+        with pytest.raises(KeyError):
+            make_all_possibilities(examples, empty, "name")
 
 
 class TestCanConcatExamples(object):
@@ -62,3 +84,125 @@ class TestCanConcatExamples(object):
         setattr(ex1, RANDGEN_MAPPING_KEY, {"randgen name": True})
         assert can_concat_examples(ex1, ex2)
         assert can_concat_examples(ex2, ex1)
+    
+    def test_mapping_compatible(self):
+        ex1 = Example("test1")
+        ex2 = Example("test2")
+
+        setattr(ex1, RANDGEN_MAPPING_KEY, {"name": True})
+        setattr(ex2, RANDGEN_MAPPING_KEY, {"name": True})
+        assert can_concat_examples(ex1, ex2)
+        assert can_concat_examples(ex2, ex1)
+
+        setattr(ex1, RANDGEN_MAPPING_KEY, {"name": False})
+        setattr(ex2, RANDGEN_MAPPING_KEY, {"name": False})
+        assert can_concat_examples(ex1, ex2)
+        assert can_concat_examples(ex2, ex1)
+
+        setattr(ex1, RANDGEN_MAPPING_KEY, {"other": True})
+        setattr(ex2, RANDGEN_MAPPING_KEY, {"name": True})
+        assert can_concat_examples(ex1, ex2)
+        assert can_concat_examples(ex2, ex1)
+
+        setattr(ex1, RANDGEN_MAPPING_KEY, {"other": False})
+        setattr(ex2, RANDGEN_MAPPING_KEY, {"name": True})
+        assert can_concat_examples(ex1, ex2)
+        assert can_concat_examples(ex2, ex1)
+
+        setattr(ex1, RANDGEN_MAPPING_KEY, {"name": False})
+        setattr(ex2, RANDGEN_MAPPING_KEY, {"name": False, "name2": True})
+        assert can_concat_examples(ex1, ex2)
+        assert can_concat_examples(ex2, ex1)
+
+        setattr(ex1, RANDGEN_MAPPING_KEY, {"name": False, "name2": True})
+        setattr(ex2, RANDGEN_MAPPING_KEY, {"name": False, "name2": True})
+        assert can_concat_examples(ex1, ex2)
+        assert can_concat_examples(ex2, ex1)
+    
+    def test_mapping_incompatible(self):
+        ex1 = Example("test1")
+        ex2 = Example("test2")
+
+        setattr(ex1, RANDGEN_MAPPING_KEY, {"name": True})
+        setattr(ex2, RANDGEN_MAPPING_KEY, {"name": False})
+        assert not can_concat_examples(ex1, ex2)
+        assert not can_concat_examples(ex2, ex1)
+
+        setattr(ex1, RANDGEN_MAPPING_KEY, {"name": False})
+        setattr(ex2, RANDGEN_MAPPING_KEY, {"name": True})
+        assert not can_concat_examples(ex1, ex2)
+        assert not can_concat_examples(ex2, ex1)
+
+        setattr(ex1, RANDGEN_MAPPING_KEY, {"name": True, "name1": True})
+        setattr(ex2, RANDGEN_MAPPING_KEY, {"name": True, "name1": False})
+        assert not can_concat_examples(ex1, ex2)
+        assert not can_concat_examples(ex2, ex1)
+
+
+class TestMergeRandgenMappings(object):
+    def test_empty_mappings(self):
+        ex1 = Example()
+        ex2 = Example()
+        assert merge_randgen_mappings(ex1, ex2) is None
+
+        ex1 = Example("test1")
+        ex2 = Example("test2")
+        assert merge_randgen_mappings(ex1, ex2) is None
+
+    def test_no_merge(self):
+        ex1 = Example("test1")
+        ex2 = Example("test2")
+
+        mapping = {"name": True}
+        setattr(ex1, RANDGEN_MAPPING_KEY, mapping)
+
+        assert merge_randgen_mappings(ex1, ex2) == mapping
+
+        ex1 = Example("test1")
+        setattr(ex2, RANDGEN_MAPPING_KEY, mapping)
+
+        assert merge_randgen_mappings(ex1, ex2) == mapping
+
+    def test_merge(self):
+        ex1 = Example("test1")
+        ex2 = Example("test2")
+
+        mapping1 = {"name": True, "other": False}
+        setattr(ex1, RANDGEN_MAPPING_KEY, mapping1)
+        mapping2 = {"other": False, "third": True}
+        setattr(ex2, RANDGEN_MAPPING_KEY, mapping2)
+
+        assert \
+            merge_randgen_mappings(ex1, ex2) == \
+                {"name": True, "other": False, "third": True}
+
+
+class TestConcatExamplesRandgen(object):
+    def test_empty(self):
+        ex1 = Example()
+        ex2 = Example()
+        assert concat_examples_with_randgen(ex1, ex2) == Example()
+
+    def test_single(self):
+        ex1 = Example("test1")
+        ex2 = Example()
+        mapping = {"name": True}
+        setattr(ex1, RANDGEN_MAPPING_KEY, mapping)
+
+        concated = concat_examples_with_randgen(ex1, ex2)
+        assert concated.text == ex1.text
+        assert getattr(concated, RANDGEN_MAPPING_KEY, None) == mapping
+    
+    def test_concat(self):
+        ex1 = Example("test1")
+        ex2 = Example("test2")
+        mapping1 = {"name": True}
+        mapping2 = {"other": False}
+        setattr(ex1, RANDGEN_MAPPING_KEY, mapping1)
+        setattr(ex2, RANDGEN_MAPPING_KEY, mapping2)
+
+        concated = concat_examples_with_randgen(ex1, ex2)
+        assert concated.text == "test1test2"
+        assert \
+            getattr(concated, RANDGEN_MAPPING_KEY, None) == \
+                {"name": True, "other": False}
