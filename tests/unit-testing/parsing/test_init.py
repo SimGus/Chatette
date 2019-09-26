2d3c8c4fae066228bf8793e700443d8edf086ade
SimGus <simon.gustin@hotmail.com>
2019-09-26 23:08:23 +0200

    Add unit tests for unit reference builder

--- tests/unit-testing/parsing/test_init.py
+++ tests/unit-testing/parsing/test_init.py
@@ -11,6 +11,15 @@ from chatette.parsing import \
     ItemBuilder, ChoiceBuilder, UnitRefBuilder, \
     UnitDefBuilder, AliasDefBuilder, SlotDefBuilder, IntentDefBuilder
 
+from chatette.utils import UnitType
+
+from chatette.modifiers.representation import ModifiersRepresentation
+from chatette.units.modifiable.choice import Choice
+from chatette.units.modifiable.unit_reference import UnitReference
+from chatette.units.modifiable.definitions.alias import AliasDefinition
+from chatette.units.modifiable.definitions.slot import SlotDefinition
+from chatette.units.modifiable.definitions.intent import IntentDefinition
+
 
 class TestItemBuilder(object):
     def test_abstract(self):
@@ -33,3 +42,46 @@ class TestChoiceBuilder(object):
 
         with pytest.raises(ValueError):
             builder.create_concrete()
+
+        builder.randgen = True
+        modifiers = builder._build_modifiers_repr()
+        assert isinstance(modifiers, ModifiersRepresentation)
+        assert not modifiers.casegen
+        assert modifiers.randgen
+        assert modifiers.randgen_name == "name"
+        assert modifiers.randgen_percent == 50
+
+        choice = builder.create_concrete()
+        assert isinstance(choice, Choice)
+        assert not choice._leading_space
+        assert len(choice._rules) == 0
+
+class TestUnitRefBuilder(object):
+    def test_creation(self):
+        builder = UnitRefBuilder()
+        assert not builder.leading_space
+        assert not builder.casegen
+        assert not builder.randgen
+        assert builder.randgen_name is None
+        assert builder.randgen_percent == 50
+
+    def test_create_concrete(self):
+        builder = UnitRefBuilder()
+        builder.identifier = "id"
+
+        with pytest.raises(ValueError):
+            builder.create_concrete()
+
+        builder.type = UnitType.alias
+        modifiers = builder._build_modifiers_repr()
+        assert isinstance(modifiers, ModifiersRepresentation)
+        assert not modifiers.casegen
+        assert not modifiers.randgen
+        assert modifiers.randgen_name is None
+        assert modifiers.randgen_percent == 50
+
+        unit_ref = builder.create_concrete()
+        assert isinstance(unit_ref, UnitReference)
+        assert not unit_ref._leading_space
+        assert unit_ref._unit_type == UnitType.alias
+        assert unit_ref._name == "id"
