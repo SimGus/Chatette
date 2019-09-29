7c8c972cfd087c983fd6de14a54be5cccfedcd94
SimGus <simon.gustin@hotmail.com>
2019-09-29 19:57:27 +0200

    Improve test coverage

--- tests/unit-testing/parsing/test_init.py
+++ tests/unit-testing/parsing/test_init.py
@@ -20,6 +20,8 @@ from chatette.units.modifiable.definitions.alias import AliasDefinition
 from chatette.units.modifiable.definitions.slot import SlotDefinition
 from chatette.units.modifiable.definitions.intent import IntentDefinition
 
+from chatette.units.ast import AST
+
 
 class TestItemBuilder(object):
     def test_abstract(self):
@@ -102,24 +104,34 @@ class TestAliasDefBuilder(object):
 
     def test_create_concrete(self):
         builder = AliasDefBuilder()
-        builder.identifier = "id"
-        builder.randgen_name = "name"
 
         with pytest.raises(ValueError):
             builder.create_concrete()
-        builder.randgen = True
+        builder.identifier = "id"
 
         modifiers = builder._build_modifiers_repr()
         assert isinstance(modifiers, ModifiersRepresentation)
         assert not modifiers.casegen
-        assert modifiers.randgen
-        assert modifiers.randgen_name == "name"
+        assert not modifiers.randgen
         assert modifiers.randgen_percent == 50
 
         alias = builder.create_concrete()
         assert isinstance(alias, AliasDefinition)
         assert not alias._leading_space
         assert alias._name == "id"
+
+    def test_new_variation(self):
+        builder = AliasDefBuilder()
+        builder.identifier = "id"
+        alias = builder.create_concrete()
+        assert "var" not in alias
+
+        ast = AST.get_or_create()
+        ast.add_alias(alias)
+
+        builder.variation = "var"
+        same_alias = builder.create_concrete()
+        assert same_alias == alias
 
 class TestSlotDefBuilder(object):
     def test_creation(self):
@@ -150,6 +162,19 @@ class TestSlotDefBuilder(object):
         assert isinstance(slot, SlotDefinition)
         assert not slot._leading_space
         assert slot._name == "id"
+
+    def test_new_variation(self):
+        builder = SlotDefBuilder()
+        builder.identifier = "id"
+        slot = builder.create_concrete()
+        assert "var" not in slot
+
+        ast = AST.get_or_create()
+        ast.add_slot(slot)
+
+        builder.variation = "var"
+        same_slot = builder.create_concrete()
+        assert same_slot == slot
 
 class TestIntentDefBuilder(object):
     def test_creation(self):
@@ -183,3 +208,16 @@ class TestIntentDefBuilder(object):
         assert intent._name == "id"
         assert intent._nb_training_ex_asked == 100
         assert intent._nb_testing_ex_asked is None
+    
+    def test_new_variation(self):
+        builder = IntentDefBuilder()
+        builder.identifier = "id"
+        intent = builder.create_concrete()
+        assert "var" not in intent
+
+        ast = AST.get_or_create()
+        ast.add_intent(intent)
+
+        builder.variation = "var"
+        same_intent = builder.create_concrete()
+        assert same_intent == intent
