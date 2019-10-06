f91c760b073a1b5e87e229106ae3b491445ad74f
SimGus <simon.gustin@hotmail.com>
2019-10-06 22:24:45 +0200

    Adapt parser to handle opposite randgen modifiers

--- chatette/parsing/parser.py
+++ chatette/parsing/parser.py
@@ -357,6 +357,7 @@ class Parser(object):
         definition).
         Returns the rule (`Rule`) that `tokens` represent.
         """
+        # TODO replace this with a (stateful) iterator to make it more readable
         rule_contents = []
         current_builder = None
         leading_space = False
@@ -406,7 +407,7 @@ class Parser(object):
                 current_builder = ChoiceBuilder()
                 current_builder.leading_space = leading_space
                 last_internal_choice_token = \
-                    utils.index_end_choice_rules(tokens, i)
+                    utils.find_index_last_choice_content(tokens, i)
                 if last_internal_choice_token is not None:
                     i += 1
                     if tokens[i].type == TerminalType.casegen_marker:
@@ -420,7 +421,7 @@ class Parser(object):
                     i = last_internal_choice_token
                 else:
                     self.input_file_manager.syntax_error(
-                        "Inconsistent choice starts and endings."
+                        "Inconsistent choice start and ending."
                     )
             elif token.type == TerminalType.choice_end:
                 rule_contents.append(current_builder.create_concrete())
@@ -431,6 +432,8 @@ class Parser(object):
                 current_builder.casegen = True
             elif token.type == TerminalType.randgen_marker:
                 current_builder.randgen = True
+            elif token.type == TerminalType.opposite_randgen_marker:
+                current_builder.randgen_opposite = True
             elif token.type == TerminalType.randgen_name:
                 current_builder.randgen_name = token.text
             elif token.type == TerminalType.percentgen_marker:
